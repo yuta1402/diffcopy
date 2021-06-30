94fe89c35e11ca3770d280550ea776a89844d405
guts <yuta1402souja@live.jp>
2021-07-01 02:53:43 +0900

    Add support for command line

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yuta1402/diffcopy
 
 go 1.16
+
+require github.com/cheggaaa/pb/v3 v3.0.8
--- main.go
+++ main.go
@@ -1,9 +1,67 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/cheggaaa/pb/v3"
+	"github.com/yuta1402/diffcopy/pkg/diffcopy"
 )
 
 func main() {
-	fmt.Printf("Hello, world\n")
+	flag.Usage = func() {
+		usageText := `Usage:
+    diffcopy [options] <src> <dest>
+Options:
+`
+		fmt.Fprint(flag.CommandLine.Output(), usageText)
+		flag.PrintDefaults()
+	}
+
+	var (
+		outDir string
+	)
+
+	flag.StringVar(&outDir, "output", "", "output directory ( default: <dest> )")
+
+	flag.Parse()
+
+	if flag.NArg() < 2 {
+		flag.Usage()
+		return
+	}
+
+	args := flag.Args()
+
+	var (
+		srcDir  = args[0]
+		destDir = args[1]
+	)
+
+	if outDir == "" {
+		outDir = destDir
+	}
+
+	files, err := diffcopy.FindWaitingFiles(srcDir, destDir)
+	if err != nil {
+		return
+	}
+
+	bar := pb.StartNew(len(files))
+
+	for _, f := range files {
+		p, _ := filepath.Rel(srcDir, f)
+		outPath := filepath.Join(outDir, p)
+
+		if err := diffcopy.CopyFile(f, outPath); err != nil {
+			fmt.Fprintf(os.Stderr, "%s", err)
+			os.Exit(1)
+		}
+
+		bar.Increment()
+	}
+
+	bar.Finish()
 }
